0b456a7a1880b291d7dcd8398e46bbd4b4c51c0f
Noemi Ashizuka <noemi.calanducci@gmail.com>
2021-05-10 09:29:30 +0900

    finished logic for the big cup

--- script.js
+++ script.js
@@ -3,6 +3,8 @@ const percentage = document.getElementById('percentage');
 const liters = document.getElementById('liters');
 const waterLeft = document.getElementById('water-left');
 
+updateBigCup();
+
 cups.forEach((cup, index) => {
   cup.addEventListener('click', () => selectCups(index))
 });
@@ -19,4 +21,29 @@ const selectCups = (index) => {
       cup.classList.remove('full')
     };
   });
+
+  updateBigCup();
 };
+
+function updateBigCup() {
+  const fullCups = document.querySelectorAll('.cup-small.full').length;
+
+  const totalCups = cups.length;
+  
+  if(fullCups === 0) {
+    percentage.style.visibility = 'hidden';
+    percentage.style.height = 0;
+  } else {
+    percentage.style.visibility = 'visible';
+    percentage.style.height = `${fullCups / totalCups * 330}px`;
+    percentage.innerText = `${fullCups / totalCups * 100}%`
+  }
+
+  if(fullCups === totalCups){
+    waterLeft.style.visibility = 'hidden';
+    waterLeft.style.height = 0;
+  } else {
+    waterLeft.style.visibility = 'visible';
+    liters.innerText = `${2 - (250 * fullCups / 1000)}L`
+  }
+}
